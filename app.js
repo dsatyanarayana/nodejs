04ea223262d27dcca0bb85af4a10a8e5f446de0f
= <satyanarayana.dhangeti@gmail.com>
2024-08-19 15:37:05 +0530

    Common JS Module

---
+++ app.js
@@ -0,0 +1,5 @@
+const { sum,name } = require("./calculations.js");
+const a = 10;
+const b = 40;
+sum(a,b);
+console.log("My name is "+name);
---
+++ calculations.js
@@ -0,0 +1,8 @@
+console.log("Entered Calculations");
+function sum(a, b) {
+  let result = a + b;
+  console.log("Result is " + result);
+}
+const name = "D SATYANARAYANA";
+
+module.exports = { sum };
