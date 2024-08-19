d7086e48e942a7f9d68d1e37a124e864b4cad976
= <satyanarayana.dhangeti@gmail.com>
2024-08-19 15:53:25 +0530

    ESM Modules

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const { sum,name } = require("./calculations.js");
+// const { sum,name } = require("./calculations.js");
+// const a = 10;
+// const b = 40;
+// sum(a,b);
+// console.log("My name is "+name);
+
+//ESM Modules or ES6
+import {sum,name} from './calculations.js';
 const a = 10;
 const b = 40;
 sum(a,b);
--- calculations.js
+++ calculations.js
@@ -1,8 +1,15 @@
-console.log("Entered Calculations");
-function sum(a, b) {
-  let result = a + b;
-  console.log("Result is " + result);
-}
-const name = "D SATYANARAYANA";
+// console.log("Entered Calculations");
+// function sum(a, b) {
+//   let result = a + b;
+//   console.log("Result is " + result);
+// }
+// const name = "D SATYANARAYANA";
 
-module.exports = { sum };
+// module.exports = { sum,name };
+
+export const name = "D SATYANARAYANA";
+export function sum(a,b){
+    const result = a+b;
+    console.log("Result is "+result);
+}
+
